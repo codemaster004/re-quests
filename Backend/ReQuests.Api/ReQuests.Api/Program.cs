436e94c282bb49a740cb96ae31b5e9880cc3e07a
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2021-12-06 15:12:31 +0100

    created api project

---
+++ Backend/ReQuests.Api/ReQuests.Api/Program.cs
@@ -0,0 +1,33 @@
+using ReQuests.Data;
+
+var builder = WebApplication.CreateBuilder( args );
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddNpgsql<AppDbContext>(
+	builder.Configuration.GetConnectionString( "postgresConnection" ), 
+	 pgob => pgob.MigrationsAssembly( "ReQuests.Migrations.Pg" ),
+	 ob => ob.UseLoggerFactory( LoggerFactory.Create( factoryBuilder => factoryBuilder.AddConsole() ) )
+	 );
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if ( app.Environment.IsDevelopment() )
+{
+	_ = app.UseSwagger();
+	_ = app.UseSwaggerUI();
+}
+
+_ = app.UseHttpsRedirection();
+
+_ = app.UseAuthorization();
+
+_ = app.MapControllers();
+
+app.Run();
