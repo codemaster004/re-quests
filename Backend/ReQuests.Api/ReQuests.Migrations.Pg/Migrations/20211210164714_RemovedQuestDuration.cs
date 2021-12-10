b57894adec6556f38cb8ae28e7ce38025664e938
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2021-12-10 19:34:51 +0100

    remapped database timestamps

---
+++ Backend/ReQuests.Api/ReQuests.Migrations.Pg/Migrations/20211210164714_RemovedQuestDuration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReQuests.Migrations.Pg.Migrations
+{
+    public partial class RemovedQuestDuration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			_ = migrationBuilder.DropColumn(
+				name: "Duration",
+				table: "Quests" );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+			_ = migrationBuilder.AddColumn<string>(
+				name: "Duration",
+				table: "Quests",
+				type: "text",
+				nullable: false,
+				defaultValue: "" );
+        }
+    }
+}
---
+++ Backend/ReQuests.Api/ReQuests.Migrations.Pg/Migrations/20211210164902_ReaddedQuestDuration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReQuests.Migrations.Pg.Migrations
+{
+    public partial class ReaddedQuestDuration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			_ = migrationBuilder.AddColumn<TimeSpan>(
+				name: "Duration",
+				table: "Quests",
+				type: "interval",
+				nullable: false,
+				defaultValue: new TimeSpan( 0, 0, 0, 0, 0 ) );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+			_ = migrationBuilder.DropColumn(
+				name: "Duration",
+				table: "Quests" );
+        }
+    }
+}
